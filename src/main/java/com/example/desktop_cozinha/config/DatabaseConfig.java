8a8972ef53e2340a9798e0ed464225f49079e089
devguilhermerocha <dev.guilhermerocha@hotmail.com>
2026-04-27 11:11:49 -0300

    atualizando os pacotes, além da main

---
+++ src/main/java/com/example/desktop_cozinha/config/DatabaseConfig.java
@@ -0,0 +1,15 @@
+package com.example.desktop_cozinha.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConfig {
+    public static Connection getConnection() throws SQLException {
+        String url = "jdbc:mysql://localhost:3307/destopcozinha";
+        String user = "root";
+        String password = "senac";
+        return DriverManager.getConnection(url, user, password);
+    }
+}
+
--- src/main/java/com/example/desktop_cozinha/controller/LoginController.java
+++ src/main/java/com/example/desktop_cozinha/controller/LoginController.java
@@ -1,9 +1,9 @@
-package com.example.desktop_cozinha;
+package com.example.desktop_cozinha.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class LoginController {
     @FXML
     private Label welcomeText;
 
