42eaf05c21f7f0a22b746e9d2c83af0903128a64
DiegoLopes <diegolopes13072007@gmail.com>
2026-04-28 08:24:54 -0300

    Finaliza EsqueciSenha feature

--- src/main/java/com/example/desktop_cozinha/Services/SessaoService.java
+++ src/main/java/com/example/desktop_cozinha/Services/SessaoService.java
@@ -7,7 +7,7 @@ public class SessaoService {
     public static String emailAtual;
 
 
-      7 public static String getEmailAtual() {return emailAtual;}
+       public static String getEmailAtual() {return emailAtual;}
        public static void setEmailAtual(String emailAtual) {SessaoService.emailAtual = emailAtual;}
 
 
