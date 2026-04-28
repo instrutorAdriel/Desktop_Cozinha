997704cd0299de529ac8456b4d1fb44c3224ea86
lucasc14 <lucas.c14.51@gmail.com>
2026-04-28 10:41:05 -0300

    Cria metodo cadastrar

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.desktop_cozinha {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires jbcrypt;
 
 
     opens com.example.desktop_cozinha.controller to javafx.fxml;
