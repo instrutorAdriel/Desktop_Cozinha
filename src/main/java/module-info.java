0ae1964b759cabae2cd9ac7284e7cf475833d5de
devguilhermerocha <dev.guilhermerocha@hotmail.com>
2026-04-28 09:01:34 -0300

    correção do modulo

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.example.desktop_cozinha {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
-    opens com.example.desktop_cozinha to javafx.fxml;
+    opens com.example.desktop_cozinha.controller to javafx.fxml;
     exports com.example.desktop_cozinha;
 }
