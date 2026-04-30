3382cc2ca3686058c4973d58eab3beba837cbedd
Mateus <mateus60093616@edu.df.senac.br>
2026-04-30 10:56:43 -0300

    desenvolvido tela esqueceu senha

--- src/main/java/com/example/desktop_cozinha/MainApplication.java
+++ src/main/java/com/example/desktop_cozinha/MainApplication.java
@@ -15,7 +15,7 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         primaryStage = stage;
 
-        trocadorDeTelas("login.fxml");
+        trocadorDeTelas("esqueceu-senha.fxml");
 
         primaryStage.setTitle("Sistema Cozinha");
         primaryStage.show();
