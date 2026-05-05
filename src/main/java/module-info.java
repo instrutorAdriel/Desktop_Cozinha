6156a94775ac2e9a38c6b9671b9766f263ee5709
devguilhermerocha <dev.guilhermerocha@hotmail.com>
2026-05-05 09:42:28 -0300

    adicionando uma nova requesitos nos modulos

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,11 @@
 module com.example.desktop_cozinha {
     requires javafx.controls;
     requires javafx.fxml;
-    
     requires java.sql;
     requires jbcrypt;
     requires jdk.compiler;
     requires mysql.connector.j;
+    requires jakarta.mail;
 
     opens com.example.desktop_cozinha to javafx.fxml;
     exports com.example.desktop_cozinha;
