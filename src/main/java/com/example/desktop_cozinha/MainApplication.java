b92580d6373537ab589810636e4835fd418a4c35
deysielenp <deysielenp@gmail.com>
2026-05-05 11:56:53 -0300

    Iniciando a tela de configuracoes

--- src/main/java/com/example/desktop_cozinha/MainApplication.java
+++ src/main/java/com/example/desktop_cozinha/MainApplication.java
@@ -15,7 +15,7 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         primaryStage = stage;
 
-        trocadorDeTelas("cadastro.fxml");
+        trocadorDeTelas("login.fxml");
 
         primaryStage.setTitle("Sistema Cozinha");
         primaryStage.show();
