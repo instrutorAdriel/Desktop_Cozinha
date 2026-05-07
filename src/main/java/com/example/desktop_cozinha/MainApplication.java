1bff78b0e6e06621ac142a62065b0567512403a3
Deysi <deysielenp@gmail.com>
2026-05-07 17:45:37 -0300

    Finalização completa da tela de cadastro de usuarios

--- src/main/java/com/example/desktop_cozinha/MainApplication.java
+++ src/main/java/com/example/desktop_cozinha/MainApplication.java
@@ -15,7 +15,7 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         primaryStage = stage;
 
-        trocadorDeTelas("login.fxml");
+        trocadorDeTelas("AdicionarPoup.fxml");
 
         primaryStage.setTitle("Sistema Cozinha");
         primaryStage.show();
