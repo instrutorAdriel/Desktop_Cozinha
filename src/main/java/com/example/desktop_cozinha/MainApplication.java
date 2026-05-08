5785fe260402128c476c0b3c41b78e0b2f5d7f1b
Deysi <deysielenp@gmail.com>
2026-05-07 23:50:11 -0300

    Finalização da tela de relatório, adição de poupus's

--- src/main/java/com/example/desktop_cozinha/MainApplication.java
+++ src/main/java/com/example/desktop_cozinha/MainApplication.java
@@ -15,7 +15,7 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         primaryStage = stage;
 
-        trocadorDeTelas("login.fxml");
+        trocadorDeTelas("SaidaPoup.fxml");
 
         primaryStage.setTitle("Sistema Cozinha");
         primaryStage.show();
---
+++ src/main/java/com/example/desktop_cozinha/controller/SolicitacaoController.java
@@ -0,0 +1,4 @@
+package com.example.desktop_cozinha.controller;
+
+public class SolicitacaoController {
+}
