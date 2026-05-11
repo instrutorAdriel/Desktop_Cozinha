47d5e938e605752c353a9cef74dd6dbe31af2989
deysielenp <deysielenp@gmail.com>
2026-05-11 09:12:30 -0300

    Modificaçãso de alguns componentes

--- src/main/java/com/example/desktop_cozinha/MainApplication.java
+++ src/main/java/com/example/desktop_cozinha/MainApplication.java
@@ -15,7 +15,7 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         primaryStage = stage;
 
-        trocadorDeTelas("login.fxml");
+        trocadorDeTelas("configuracao.fxml");
 
         primaryStage.setTitle("Sistema Cozinha");
         primaryStage.show();
