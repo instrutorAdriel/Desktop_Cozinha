a0b1408cc0d83b7b4f1a8506f288e488b4a875ca
Claudia de Sousa Lima <claudia32967876@edu.df.senac.br>
2026-05-13 11:02:24 -0300

    ajustando tamanho das telas

--- src/main/java/com/example/desktop_cozinha/MainApplication.java
+++ src/main/java/com/example/desktop_cozinha/MainApplication.java
@@ -26,7 +26,7 @@ public class MainApplication extends Application {
                 MainApplication.class.getResource(fxml)
         );
 
-        Scene scene = new Scene(fxmlLoader.load());
+        Scene scene = new Scene(fxmlLoader.load(), 1366,768);
 
         primaryStage.setScene(scene);
     }
