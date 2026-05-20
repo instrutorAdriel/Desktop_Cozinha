e04b402520a1a06fa4af243a4e3436eea904aa20
devguilhermerocha <dev.guilhermerocha@hotmail.com>
2026-05-20 11:44:45 -0300

    Foi desenvolvido a função de Notificação/Alerta de produtos em vecimento ou estoque baixo, funcionando legal

--- src/main/java/com/example/desktop_cozinha/controller/HomeController.java
+++ src/main/java/com/example/desktop_cozinha/controller/HomeController.java
@@ -1,13 +1,17 @@
 package com.example.desktop_cozinha.controller;
 
+import com.example.desktop_cozinha.model.AlertaProduto;
+import com.example.desktop_cozinha.model.AlertaProdutoDAO;
 import com.example.desktop_cozinha.model.HomeDAO;
 import com.example.desktop_cozinha.services.SessaoService;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.util.Duration;
 
 import java.awt.event.ActionEvent;
@@ -15,6 +19,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import static com.example.desktop_cozinha.MainApplication.sair;
@@ -34,8 +39,26 @@ public class HomeController implements Initializable {
     @FXML
     private Button botaoSair;
 
+
+    @FXML
+    private TableView<AlertaProduto> tabelaAlertas;
+
+    @FXML private TableColumn<AlertaProduto, String> colProduto;
+    @FXML private TableColumn<AlertaProduto, String> colValidade;
+    @FXML private TableColumn<AlertaProduto, String> colStatus;
+    @FXML private TableColumn<AlertaProduto, String> colQuantidade;
+
+    private final AlertaProdutoDAO alertaDAO = new AlertaProdutoDAO();
+    private final ObservableList<AlertaProduto> listaAlertas = FXCollections.observableArrayList();
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        colProduto.setCellValueFactory(new PropertyValueFactory<>("nome"));
+        colValidade.setCellValueFactory(new PropertyValueFactory<>("dataValidade"));
+        colStatus.setCellValueFactory(new PropertyValueFactory<>("status"));
+        colQuantidade.setCellValueFactory(new PropertyValueFactory<>("quantidade"));
+        configurarEstiloLinhas();
+        atualizarTabela();
         configurarRelogio();
         usuarioAtual();
     }
@@ -66,6 +89,22 @@ public class HomeController implements Initializable {
             String nome = user.bucarNome(email);
             usuarioLabel.setText("Bem vindo, " + nome);
         }
+    }
+
+    @FXML
+    public void atualizarTabela() {
+        listaAlertas.clear();
+        List<AlertaProduto> dadosDoBanco = alertaDAO.buscarProdutosEmAlerta();
+        listaAlertas.addAll(dadosDoBanco);
+        tabelaAlertas.setItems(listaAlertas);
+    }
+    private void configurarEstiloLinhas() {
+        tabelaAlertas.setRowFactory(tv -> new TableRow<>() {
+            @Override
+            protected void updateItem(AlertaProduto item, boolean empty) {
+                super.updateItem(item, empty);
+            }
+        });
     }
 
     public void botaoEstoqueAction() throws IOException {
